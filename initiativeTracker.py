eb2733a691fad039e84b85ef04f01597255c798b
Izaaq <izaaqizham@hotmail.com>
2024-11-27 01:02:17 +0800

    Updated TODO

--- initiativeTracker.py
+++ initiativeTracker.py
@@ -1,11 +1,16 @@
 """
 Initiative Tracker by Izaaq Ahmad Izham
 TODO:
-- Make UI to allow for easily adding new characters/monsters, like a small window for a form.
+- Make UI to allow for easily adding new characters/monsters, like a small window for a form. [DONE]
 - Make UI for main application [DONE]
-- Have a library of pre-made monsters
 - Use a .json file to store player and monster data [DONE]
 - Maybe monsters don't have checkbox, but when clicked on in "Queue" they are removed? Clicking just adds another monster [DONE]
+- Have a library of pre-made monsters
+- Be able to view each character's HP and AC, able to update HP
+- Look at suggestions here: https://www.reddit.com/r/DMAcademy/comments/138kaxp/looking_for_suggestions_for_an_initiative_tracker/
+- Hovering over a character reveals more details; ie saving throws, other stats, CR...
+- Add CR at the top, allows to calculate difficulty of the encounter
+- Allow current players to be edited
 """
 
 import json

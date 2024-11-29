2b787c2821af79273914fa401f8c6a2bb745f60e
Izaaq <60147050+Izaaq@users.noreply.github.com>
2024-11-29 19:37:43 +0800

    Update initiativeTracker.py

--- initiativeTracker.py
+++ initiativeTracker.py
@@ -1,16 +1,9 @@
 """
 Initiative Tracker by Izaaq Ahmad Izham
-TODO:
-- Make UI to allow for easily adding new characters/monsters, like a small window for a form. [DONE]
-- Make UI for main application [DONE]
-- Use a .json file to store player and monster data [DONE]
-- Maybe monsters don't have checkbox, but when clicked on in "Queue" they are removed? Clicking just adds another monster [DONE]
-- Have a library of pre-made monsters
-- Be able to view each character's HP and AC, able to update HP
-- Look at suggestions here: https://www.reddit.com/r/DMAcademy/comments/138kaxp/looking_for_suggestions_for_an_initiative_tracker/
-- Hovering over a character reveals more details; ie saving throws, other stats, CR...
-- Add CR at the top, allows to calculate difficulty of the encounter
-- Allow current players to be edited
+
+To use, add players by checking their name, or remove them by unchecking. 
+Then add monsters by clicking. Clicking the same monster again adds another monster to the list. Remove monsters by clicking on their name in the queue.
+After all characters are added, click "Roll for initiaitve", the Initiative list will show who goes first from top to down, as well as the number they rolled.
 """
 
 import json
